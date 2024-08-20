71b61077a447bf5e7850c44aa73814744910896d
Michael Zeller <michaelzllr1@gmail.com>
2024-08-20 10:44:55 -0500

    Register page and routes updated

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
+import RegisterPage from "./RegisterPage";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/" element={<Navigate replace to="/login" />} />
