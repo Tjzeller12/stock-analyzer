fd5af8a53bb2cd81196f0f7acdd29700e7459f32
Michael Zeller <michaelzllr1@gmail.com>
2024-08-24 19:14:23 -0500

    Adjusted main css
    Made My Stocks and News have scroll feature when an overflow occurs and changed the search button to say add

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -11,7 +11,6 @@ interface Stock {
   price: number;
   pe_ratio: number;
   dividend: number;
-
 }
 
 interface Article {
@@ -44,18 +43,168 @@ const MainPage: React.FC = () => {
   };
   // Placeholder data for stocks
   const stocks: Stock[] = [
-      { symbol: "AAPL", name: "Apple Inc.", rating: 84, safety: 98, price: 150.25, pe_ratio: 4, dividend: .7 },
-      { symbol: "GOOGL", name: "Alphabet Inc.", rating: 83, safety: 99, price: 2750.80, pe_ratio: 20, dividend: .4 },
-      { symbol: "MSFT", name: "Microsoft Corporation", rating: 90, safety: 100, price: 305.15, pe_ratio: 10, dividend: .6 },
-      { symbol: "AMZN", name: "Amazon.com, Inc.", rating: 78, safety: 95, price: 3300.50, pe_ratio: 35.5, dividend: .0 },
-      { symbol: "TSLA", name: "Tesla, Inc.", rating: 73, safety: 67, price: 780.90, pe_ratio: 80.2, dividend: .7 },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "AAPL",
+      name: "Apple Inc.",
+      rating: 84,
+      safety: 98,
+      price: 150.25,
+      pe_ratio: 4,
+      dividend: 0.7,
+    },
+    {
+      symbol: "GOOGL",
+      name: "Alphabet Inc.",
+      rating: 83,
+      safety: 99,
+      price: 2750.8,
+      pe_ratio: 20,
+      dividend: 0.4,
+    },
+    {
+      symbol: "MSFT",
+      name: "Microsoft Corporation",
+      rating: 90,
+      safety: 100,
+      price: 305.15,
+      pe_ratio: 10,
+      dividend: 0.6,
+    },
+    {
+      symbol: "AMZN",
+      name: "Amazon.com, Inc.",
+      rating: 78,
+      safety: 95,
+      price: 3300.5,
+      pe_ratio: 35.5,
+      dividend: 0.0,
+    },
+    {
+      symbol: "TSLA",
+      name: "Tesla, Inc.",
+      rating: 73,
+      safety: 67,
+      price: 780.9,
+      pe_ratio: 80.2,
+      dividend: 0.7,
+    },
   ];
   // Placeholder data for news articles
   const articles: Article[] = [
-    {image_link: "somelink", link:"somelink", title:"Elon Musk tanks META stock down 20%", news_company:'X'},
-    {image_link: "somelink", link:"somelink", title:"Joe Rogan, is Joe Hogans little brother which has caused GameStop to crash?", news_company:'CNN'},
-    {image_link: "somelink", link:"somelink", title:"Donald Trump and Khamala Harris are having a baby? NVDA to the moon", news_company:'Fox'}
-  ]
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title:
+        "Joe Rogan, is Joe Hogans little brother which has caused GameStop to crash?",
+      news_company: "CNN",
+    },
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title:
+        "Joe Rogan, is Joe Hogans little brother which has caused GameStop to crash?",
+      news_company: "CNN",
+    },
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title:
+        "Joe Rogan, is Joe Hogans little brother which has caused GameStop to crash?",
+      news_company: "CNN",
+    },
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title: "Elon Musk tanks META stock down 20%",
+      news_company: "X",
+    },
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title:
+        "Joe Rogan, is Joe Hogans little brother which has caused GameStop to crash?",
+      news_company: "CNN",
+    },
+    {
+      image_link: "somelink",
+      link: "somelink",
+      title:
+        "Donald Trump and Khamala Harris are having a baby? NVDA to the moon",
+      news_company: "Fox",
+    },
+  ];
 
   return (
     <div className="main-container">
@@ -70,8 +219,7 @@ const MainPage: React.FC = () => {
         />
       </header>
 
-      
-        {/* Navigation buttons for additional features */}
+      {/* Navigation buttons for additional features */}
       <div className="button-container">
         {[
           { label: "Profile", path: "/profile" },
@@ -99,7 +247,7 @@ const MainPage: React.FC = () => {
             value={searchSymbol}
             onChange={(e) => setSearchSymbol(e.target.value)}
           />
-          <button type="submit">Search</button>
+          <button type="submit">Add</button>
         </form>
       </div>
 
@@ -109,35 +257,44 @@ const MainPage: React.FC = () => {
         <div className="stock-container">
           <h2 className="my-stocks-title">My Stocks</h2>
           <div className="stocks-list">
-                {stocks.map((stock, index) => (
-                    <div key={stock.symbol} className={`stock-row ${index % 2 === 0 ? 'even' : 'odd'}`}>
-                        <div className="stock-symbol">{stock.symbol}</div>
-                        <div className="stock-name">{stock.name}</div>
-                        <div className="stock-rating">{stock.rating.toFixed(2)}</div>
-                        <div className="stock-safety">{stock.safety.toFixed(2)}</div>
-                        <div className="stock-price">${stock.price.toFixed(2)}</div>
-                        <div className="stock-pe_ratio">{stock.pe_ratio.toFixed(1)}</div>
-                        <div className="stock-dividend">{stock.dividend.toFixed(1)}%</div>
-                    </div>
-                ))}
-            </div>
+            {stocks.map((stock, index) => (
+              <div
+                key={stock.symbol}
+                className={`stock-row ${index % 2 === 0 ? "even" : "odd"}`}
+              >
+                <div className="stock-symbol">{stock.symbol}</div>
+                <div className="stock-name">{stock.name}</div>
+                <div className="stock-rating">{stock.rating.toFixed(2)}</div>
+                <div className="stock-safety">{stock.safety.toFixed(2)}</div>
+                <div className="stock-price">${stock.price.toFixed(2)}</div>
+                <div className="stock-pe_ratio">
+                  {stock.pe_ratio.toFixed(1)}
+                </div>
+                <div className="stock-dividend">
+                  {stock.dividend.toFixed(1)}%
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         {/* news links for news that relates to the users stocks */}
-          <div className="news-container">
-            <h2 className="news-title">News</h2>
-            <div className="news-list">
-              {articles.map((article, index) => (
-                <div key={article.link} className={`article-row ${index % 2 === 0 ? 'even' : 'odd'}`} onClick={() => window.open(article.link, '_blank')}>
-                    <img src={article.image_link}></img>
-                    <div className="article-title">{article.title} </div>
-                    <div className="news-company">{article.news_company} </div>
-                </div>
-              ))}
-            </div>
-        
+        <div className="news-container">
+          <h2 className="news-title">News</h2>
+          <div className="news-list">
+            {articles.map((article, index) => (
+              <div
+                key={article.link}
+                className={`article-row ${index % 2 === 0 ? "even" : "odd"}`}
+                onClick={() => window.open(article.link, "_blank")}
+              >
+                <img src={article.image_link}></img>
+                <div className="article-title">{article.title} </div>
+                <div className="news-company">{article.news_company} </div>
+              </div>
+            ))}
           </div>
+        </div>
       </div>
-
     </div>
   );
 };
